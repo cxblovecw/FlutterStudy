c5d1943290d3f8ebc4da88b615e3e9cd13c731a3
cxblovecw <873587091@qq.com>
2020-05-21 15:07:43 +0800

    voice1.0

--- lib/pages/chat/parts/chat-bar-content/chat-voice.dart
+++ lib/pages/chat/parts/chat-bar-content/chat-voice.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class ChatVoice extends StatefulWidget {
   @override
   _ChatVoiceState createState() => _ChatVoiceState();
@@ -19,15 +17,17 @@ class _ChatVoiceState extends State<ChatVoice> {
     "松手转文字",
     "松手取消发送",
   ];
-  // 语音转文字的按钮
+  // 语音转文字的按钮颜色
   Color voiceToTxt=Colors.black12;
-  // 取消发送
+  // 取消发送的按钮颜色
   Color cancel=Colors.black12;
   @override
   Widget build(BuildContext context) {
+    // 获取屏幕宽高
      double width=MediaQuery.of(context).size.width;
      double height=MediaQuery.of(context).size.height;
      return Container(
+      // 大致和输入法一样高的高度
       height: MediaQuery.of(context).size.height*0.36,
       width: double.infinity,
       child: Row(
@@ -40,9 +40,11 @@ class _ChatVoiceState extends State<ChatVoice> {
                 margin: EdgeInsets.fromLTRB(width/6-30, 0, width/6-30, 0),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
+                  // voiceToTxt用于控制按钮的颜色
                   color: voiceToTxt,
                 ),
                 child: IconButton(icon: Icon(Icons.translate),onPressed: (){}),
+             // 根据opacity的值控制按钮的显示
             ),opacity: opacity)
           ),
           Expanded(
@@ -50,41 +52,48 @@ class _ChatVoiceState extends State<ChatVoice> {
               child:Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
+                  // 提示信息 根据currentIndex而改变
                   Text(hint[currentIndex],),
                   Container(height:height*0.18-60),
                   GestureDetector(
+                    // 录音按钮长按事件
                     onLongPress: (){
-                      print("长按中");
                       setState(() {
                         opacity=1;
                         currentIndex=1;
                       });
                     },
+                    // 录音按钮移动事件
                     // 注意：移动时候左边位置 是从按钮的左边开始的
                     onLongPressMoveUpdate: (value){
                       var offset=value.localPosition;
+                      // 移动到左边的 语音转文字按钮
                       if((offset.dx>((-width/2)+70))&&(offset.dx<(-width/6+30))&&(offset.dy>15)&&(offset.dy<70)){
                         setState(() {
-                          print("语音转文字");
+                          // print("语音转文字");
                           currentIndex=2;
                           voiceToTxt=Colors.green;
                         });
-                      }else if((offset.dy>15)&&(offset.dy<70)&&(offset.dx>50+width/6)&&(offset.dx<width/3+50)){
-                        print("取消发送");
+                      }
+                      // 移动到右边的取消按钮
+                      else if((offset.dy>15)&&(offset.dy<70)&&(offset.dx>50+width/6)&&(offset.dx<width/3+50)){
+                        // print("取消发送");
                         setState(() {
                           currentIndex=3;
                           cancel=Colors.red;
                         });
+                        // 长按状态 但不在两侧任意一个按钮范围内
                       }else{
                         setState(() {
                           currentIndex=1;
                           voiceToTxt=Colors.black12;
+                          cancel=Colors.black12;
                         });
                       }
                      
                     },
+                    // 录音按钮结束事件
                     onLongPressEnd: (value){
-                      print(value.localPosition);
                       setState(() {
                         opacity=0;
                         currentIndex=0;
@@ -114,9 +123,11 @@ class _ChatVoiceState extends State<ChatVoice> {
                 margin: EdgeInsets.fromLTRB(width/6-30, 0, width/6-30, 0),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
+                  // cancel 用于控制按钮的颜色
                   color: cancel,
                 ),
                 child: IconButton(icon: Icon(Icons.delete),onPressed: (){}),
+              // 根据opacity的值控制按钮的显示
             ),opacity: opacity)
           )
         ],
